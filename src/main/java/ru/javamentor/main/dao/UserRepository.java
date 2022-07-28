ec127b57f878a01e89fb2ee9f84f62148816ee0c
Вячеслав <smiv1995@gmail.com>
2022-07-28 14:03:03 +0300

    remove unused class

--- src/main/java/ru/javamentor/main/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.javamentor.main.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.javamentor.main.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
